8029913a2cb24df60f1962da3dfc50535fc8ad1c
diuis <diuis@yahoo.it>
2011-09-07 00:42:23 +0200

    work in progress

--- bjs-war/src/java/it/demis/gallisto/bjs/web/GameManagedBean.java
+++ bjs-war/src/java/it/demis/gallisto/bjs/web/GameManagedBean.java
@@ -11,7 +11,6 @@ import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.validation.constraints.Future;
 
 /**
  *
